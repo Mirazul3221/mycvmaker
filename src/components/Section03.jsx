c4aa1dae61e18e6fef39671e0b61aa3acff20971
Mirazul Islam <mirazul3221@gmail.com>
2024-01-18 23:49:19 +0600

    u

--- src/components/Section03.jsx
+++ src/components/Section03.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useEffect, useRef } from "react";
 
 const Section03 = () => {
-  return <div className="h-screen bg-sky-500">Section03</div>;
+  const ref = useRef(null);
+  const main = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+    const ele = ref.current;
+    const mainDiv = main.current;
+    gsap.to(ele, {
+      scrollTrigger: {
+        trigger: mainDiv,
+      },
+      x: 200,
+    });
+  }, []);
+
+  return (
+    <div ref={main} className="h-screen bg-sky-500">
+      <div ref={ref} className="w-28 h-28 bg-amber-500">
+        I am amber
+      </div>
+    </div>
+  );
 };
 
 export default Section03;
