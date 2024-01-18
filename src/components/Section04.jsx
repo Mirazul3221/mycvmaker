4cd0e1c7b81bc15563f16c45a66daa9703aca26a
Mirazul Islam <mirazul3221@gmail.com>
2024-01-19 00:06:21 +0600

    u

---
+++ src/components/Section04.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+const Section04 = () => {
+  return (
+    <div>
+      <div className="h-screen bg-gray-500">sfvzdf</div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quo
+        molestias consectetur laudantium doloremque tempora, unde dolorum,
+        voluptatibus illo hic quia asperiores necessitatibus a corporis aliquam.
+        Eveniet itaque ut doloribus.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quo
+        molestias consectetur laudantium doloremque tempora, unde dolorum,
+        voluptatibus illo hic quia asperiores necessitatibus a corporis aliquam.
+        Eveniet itaque ut doloribus.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quo
+        molestias consectetur laudantium doloremque tempora, unde dolorum,
+        voluptatibus illo hic quia asperiores necessitatibus a corporis aliquam.
+        Eveniet itaque ut doloribus.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quo
+        molestias consectetur laudantium doloremque tempora, unde dolorum,
+        voluptatibus illo hic quia asperiores necessitatibus a corporis aliquam.
+        Eveniet itaque ut doloribus.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quo
+        molestias consectetur laudantium doloremque tempora, unde dolorum,
+        voluptatibus illo hic quia asperiores necessitatibus a corporis aliquam.
+        Eveniet itaque ut doloribus.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quo
+        molestias consectetur laudantium doloremque tempora, unde dolorum,
+        voluptatibus illo hic quia asperiores necessitatibus a corporis aliquam.
+        Eveniet itaque ut doloribus.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quo
+        molestias consectetur laudantium doloremque tempora, unde dolorum,
+        voluptatibus illo hic quia asperiores necessitatibus a corporis aliquam.
+        Eveniet itaque ut doloribus.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quo
+        molestias consectetur laudantium doloremque tempora, unde dolorum,
+        voluptatibus illo hic quia asperiores necessitatibus a corporis aliquam.
+        Eveniet itaque ut doloribus.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quo
+        molestias consectetur laudantium doloremque tempora, unde dolorum,
+        voluptatibus illo hic quia asperiores necessitatibus a corporis aliquam.
+        Eveniet itaque ut doloribus.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quo
+        molestias consectetur laudantium doloremque tempora, unde dolorum,
+        voluptatibus illo hic quia asperiores necessitatibus a corporis aliquam.
+        Eveniet itaque ut doloribus.
+      </p>
+
+      <div className="h-screen bg-gray-500">sfvzdf</div>
+      <div className="h-screen bg-gray-500">sfvzdf</div>
+    </div>
+  );
+};
+
+export default Section04;
