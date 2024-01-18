7053e233315f1356544c854e55fcc85db56750df
Mirazul Islam <mirazul3221@gmail.com>
2024-01-18 21:34:04 +0600

    u

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Banner from "./components/Banner";
 import Section01 from "./components/Section01";
 import Section02 from "./components/Section02";
+import Section03 from "./components/Section03";
 import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
 import useLocoScroll from "./useLocoScroll";
@@ -13,6 +14,7 @@ function App() {
       <Banner />
       <Section01 />
       <Section02 />
+      <Section03 />
       <Footer />
     </main>
   );
---
+++ src/components/Section03.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Section03 = () => {
+  return <div className="h-screen bg-sky-500">Section03</div>;
+};
+
+export default Section03;
